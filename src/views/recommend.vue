29d925fab324fbb4e9b91152864a3444f1d01c7e
文三思 <394769450@qq.com>
2021-06-18 07:37:53 +0800

    feat: display recommend sliders

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,21 +1,32 @@
 <template>
-  <div>
-    推荐
-  </div>
+  <section class="recommend">
+    <div class="wrapper">
+      <div class="content">
+        <slider v-if="sliders.length > 0" :sliders="sliders"></slider>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { defineComponent } from 'vue'
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
 
-export default {
-  async mounted () {
-    const res = await getRecommend()
-    console.log('res', res)
+export default defineComponent({
+  components: {
+    Slider
   },
-  setup () {
-    return {}
+  data () {
+    return {
+      sliders: []
+    }
+  },
+  async mounted () {
+    const recommend = await getRecommend()
+    this.sliders = recommend.sliders
   }
-}
+})
 </script>
 
 <style lang="scss" scoped></style>
